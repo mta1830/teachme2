1c475e8b8a86c69addd637a04a0e4f0d5f7ba1b5
Miguel Torres <mta1830@gmail.com>
2017-02-28 11:49:59 -0400

    Public routes and TicketsController action placeholders

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,21 +1,49 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/', [
 
-Route::get('/', 'WelcomeController@index');
+    'as'   => 'tickets.latest',
 
-Route::get('home', 'HomeController@index');
+    'uses' => 'TicketsController@latest'
+
+]);
+
+Route::get('/populares', [
+
+    'as'   => 'tickets.popular',
+
+    'uses' => 'TicketsController@popular'
+
+]);
+
+Route::get('/pendientes', [
+
+    'as'   => 'tickets.open',
+
+    'uses' => 'TicketsController@open'
+
+]);
+
+Route::get('/tutoriales', [
+
+    'as'   => 'tickets.closed',
+
+    'uses' => 'TicketsController@closed'
+
+]);
+
+Route::get('/solicitud/{id}', [
+
+    'as'   => 'tickets.details',
+
+    'uses' => 'TicketsController@details'
+
+]);
 
 Route::controllers([
+
     'auth' => 'Auth\AuthController',
+
     'password' => 'Auth\PasswordController',
+
 ]);
