4b1b910d48ebc956c85e83e9fa4db099b2763211
Miguel Torres <mta1830@gmail.com>
2017-03-24 01:02:27 -0400

    Funcionalidad de votar y quitar voto por solicitud

--- app/Entities/Ticket.php
+++ app/Entities/Ticket.php
@@ -15,7 +15,7 @@ class Ticket extends Model
     }
 
     public function voters(){
-        return $this->belongsToMany(User::class,'ticket_votes');
+        return $this->belongsToMany(User::class,'ticket_votes')->withTimestamps();
     }
 
     public function getOpenAttribute(){
