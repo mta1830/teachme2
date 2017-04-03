3cdae7760d5616e55503f120f3259948ef29f067
Miguel Torres <mta1830@gmail.com>
2017-04-02 21:08:39 -0400

    jQuery y Laravel: Funcionalidad para votar con AJAX

--- app/Http/Controllers/VotesController.php
+++ app/Http/Controllers/VotesController.php
@@ -1,9 +1,5 @@
 <?php namespace TeachMe\Http\Controllers;
 
-use TeachMe\Entities\Ticket;
-use TeachMe\Http\Requests;
-use TeachMe\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 use TeachMe\Repositories\TicketRepository;
 use TeachMe\Repositories\VoteRepository;
@@ -19,16 +15,24 @@ class VotesController extends Controller {
         $this->voteRepository = $voteRepository;
     }
 
-    public function submit($id){
+    public function submit($id, Request $request){
 	    $ticket = $this->ticketRepository->findOrFail($id);
         $this->voteRepository->vote(currentUser(), $ticket);
+
+        if ($request->ajax()){
+            return response()->json(compact('success'));
+        }
 
 	    return redirect()->back();
     }
 
-    public function destroy($id){
+    public function destroy($id, Request $request){
         $ticket = $this->ticketRepository->findOrFail($id);
         $this->voteRepository->unvote(currentUser(), $ticket);
+
+        if ($request->ajax()){
+            return response()->json(compact('success'));
+        }
 
         return redirect()->back();
     }
